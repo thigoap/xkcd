8c8e99af54839edf05075e12420de76f772ccf71
thigoap <appthigo@gmail.com>
2020-06-30 19:18:34 -0300

    First commit.

---
+++ index.js
@@ -0,0 +1,79 @@
+let first = document.getElementById('first');
+let prev = document.getElementById('prev');
+let random = document.getElementById('random');
+let next = document.getElementById('next');
+let last = document.getElementById('last');
+
+let title = document.getElementById('title');
+let subtitle = document.getElementById('subtitle');
+let comic = document.getElementById('comic');
+
+let lastComic;
+let comicNumber;
+
+const getOnLoad = () => {
+  event.preventDefault();
+  getComic(comicNumber);
+}
+
+const getFirst = () => {
+  comicNumber = 1;
+  getComic(comicNumber);
+}
+
+const getPrev = () => {
+  event.preventDefault();
+  if (comicNumber <= 1) { return; } 
+  else {
+    comicNumber--;
+  }
+  getComic(comicNumber);
+}
+
+const getNext = () => {
+  event.preventDefault();
+  if (comicNumber >= lastComic) { return; } 
+  else {
+    comicNumber++;
+  }
+  getComic(comicNumber);
+}
+
+const getRandom = () => {
+  event.preventDefault();
+    let random = Math.floor(Math.random() * lastComic);
+    comicNumber = random;
+  getComic(comicNumber);
+}
+
+const getLast = () => {
+  event.preventDefault();
+  comicNumber = lastComic;
+  getComic(comicNumber);
+}
+
+const getComic = (number) => {
+  event.preventDefault();
+  if (!number) { url = `http://xkcd.com/info.0.json` }
+  else { url = `http://xkcd.com/${number}/info.0.json`; }
+  console.log(url);
+  fetch(url)
+  .then ( response => response.json() )
+  .then (result => {
+    if (!number) { lastComic = result.num; } // only on load, to define last comic
+    comicNumber = result.num;
+    title.innerHTML = `${comicNumber} - ${result.title}`;
+    subtitle.innerHTML = `<a target="blank" 
+    href="https://xkcd.com/${comicNumber}/">(see the original at XKCD)</a>`;
+    comic.style.maxWidth = '100%';
+    comic.style.height = 'auto';
+    comic.src = result.img; 
+    console.log('result', result);
+  });  
+}
+
+first.addEventListener('click', getFirst);
+prev.addEventListener('click', getPrev);
+random.addEventListener('click', getRandom);
+next.addEventListener('click', getNext);
+last.addEventListener('click', getLast);
---
+++ readme.md
@@ -0,0 +1,22 @@
+# :book: XKCD comic viewer
+
+A simple webpage that loads the json response of [XKCD](https://xkcd.com/) comics.
+> A webcomic of romance, sarcasm, math, and language.
+
+
+## Funcionalities
+
+At the moment, it has the same funcionalities of the original page:
+- Comics can be browsed via the commands "first", "prev", "random, "next", and "last".
+
+
+## To Do
+
+- [ ] go to specific comic (by comic number);
+- [ ] download comic;
+- [ ] share comic?;
+
+
+## Check live
+
+
